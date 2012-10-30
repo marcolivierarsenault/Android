91ddfb72016462b7710222c6e83ca063b87c3529
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2012-10-30 13:17:57 -0400

    mis la liste

--- JDG ETS/src/com/example/jdg/ets/MainActivity.java
+++ JDG ETS/src/com/example/jdg/ets/MainActivity.java
@@ -1,17 +1,44 @@
 package com.example.jdg.ets;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	ArrayList<String> list = new ArrayList<String>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	File sdcard = Environment.getExternalStorageDirectory();
+
+    	//Get the text file
+    	File file = new File(sdcard,"name.txt");
+
+
+    	try {
+    	    BufferedReader br = new BufferedReader(new FileReader(file));
+    	    String line;
+
+    	    while ((line = br.readLine()) != null) {
+    	        list.add(line);
+    	    }
+    	}
+    	catch (IOException e) {
+    	    //You'll need to add proper error handling here
+    	}
+    	
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -24,19 +51,15 @@ public class MainActivity extends Activity {
     
     public void random(View view) {
     	
-    	String[] dele = {
-    			"pomme",
-    			"orange",
-    			"bacon",
-    			"chip",
-    			"peanut"
-    	    	};
+    	
+    	
+    	
     	Random r = new Random();
-    	int i1=r.nextInt(dele.length);
+    	int i1=r.nextInt(list.size());
 
     	TextView t=new TextView(this); 
 
         t=(TextView)findViewById(R.id.textView1); 
-        t.setText(dele[i1]);
+        t.setText(list.get(i1));
     }
 }
